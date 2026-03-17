cddd001d5896db919f2dfdd4313e48a752a1b98b
gaston jaureguiberry <gaston.jaureguib@gmail.com>
2026-03-17 12:53:23 -0300

    1002

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,25 @@
 const CACHE_NAME = "club-cache-v1";
 
 const urlsToCache = [
-  "/",
-  "/index.html",
-  "/css/style.css",
-  "/css/panel.css",
-  "/js/admin.js",
-  "/img/logo.png"
+  "./",
+  "./index.html",
+  "./css/style.css",
+  "./css/panel.css",
+  "./js/admin.js",
+  "./img/logo.png"
 ];
 
 self.addEventListener("install", (event) => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then((cache) => cache.addAll(urlsToCache))
+      .catch(err => console.error("Error cacheando:", err))
   );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim());
 });
 
 self.addEventListener("fetch", (event) => {
